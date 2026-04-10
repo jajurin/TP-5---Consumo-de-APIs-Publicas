ad3c638bc8abc3ab21b68058f1f36e326d7a72be
NICOLÁS JAIR JAJURIN <48861075@ort>
2026-04-10 10:35:32 -0300

    Terminado, falta mensja de error

---
+++ index.js
@@ -0,0 +1,32 @@
+async function obtenerPokemon(nombre) {
+  const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+  if (!respuesta.ok) throw new Error(respuesta.status);
+
+  const data = await respuesta.json();
+  return {
+    nombrePok: data.name,
+    imagen: data.sprites.front_default,
+    tipos: data.types,
+    peso: data.weight,
+    altura: data.height
+  };
+}
+
+async function mostrarPokemon() {
+  document.getElementById("pokemonForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const nombre = e.target.pokemonName.value;
+    const pokemon = await obtenerPokemon(nombre);
+
+    document.getElementById("resultado").innerHTML = `
+      <h1>${pokemon.nombrePok}</h1>
+      <img src="${pokemon.imagen}" alt="${pokemon.nombrePok}" />
+      <p>Peso: ${pokemon.peso/10 + "kg"}</p>
+      <p>Altura: ${pokemon.altura*10 + "cm"}</p>
+      <p>Tipos: ${pokemon.tipos.map(tipo => tipo.type.name).join(", ")}</p>
+    `;
+  });
+}
+
+mostrarPokemon();
