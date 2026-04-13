459b6525a9d9ea010f10c6e567b11944e2eabffa
NICOLÁS JAIR JAJURIN <48861075@ort>
2026-04-13 12:05:08 -0300

    cambiando css y js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 async function obtenerPokemon(nombre) {
   const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
-  if (!respuesta.ok) throw new Error(respuesta.status);
+
+  if (!respuesta.ok) throw new Error("No encontrado");
 
   const data = await respuesta.json();
+
   return {
     nombrePok: data.name,
     imagen: data.sprites.front_default,
@@ -13,36 +15,25 @@ async function obtenerPokemon(nombre) {
 }
 
 async function mostrarPokemon() {
-  document.getElementById("pokemonForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
+  const nombre = document.getElementById("pokemonNombre").value;
+  const resultado = document.getElementById("resultado");
 
-    const nombre = e.target.pokemonName.value;
-    if (nombre == "") {
-  document.getElementById("resultado").innerHTML = `
-    <h2>Porfavor ingresar datos</h2>
-    <p>Ingresá un nombre o ID de Pokémon.</p>
-  `;
-  return;
-}
-    try{
-
-
+  if (nombre === "") {
+    resultado.innerHTML = `<h2>Ingresá un nombre/ID. No deje el campo vacio porfavor</h2>`;
+    return;
+  }
+  resultado.innerHTML = `<h2>Cargando...</h2>`;
+  try {
     const pokemon = await obtenerPokemon(nombre);
 
-    document.getElementById("resultado").innerHTML = `
+    resultado.innerHTML = `
       <h1>${pokemon.nombrePok}</h1>
-      <img src="${pokemon.imagen}" alt="${pokemon.nombrePok}" />
-      <p>Peso: ${pokemon.peso/10 + "kg"}</p>
-      <p>Altura: ${pokemon.altura*10 + "cm"}</p>
-      <p>Tipos: ${pokemon.tipos.map(tipo => tipo.type.name).join(", ")}</p>
+      <img src="${pokemon.imagen}" />
+      <p>Peso: ${pokemon.peso / 10} kg</p>
+      <p>Altura: ${pokemon.altura * 10} cm</p>
+      <p>Tipos: ${pokemon.tipos.map(t => t.type.name).join(", ")}</p>
     `;
-        } catch (error){
-             document.getElementById("resultado").innerHTML = `
-        <h2> Pokémon no encontrado</h2>
-        <p>Verificá el nombre e intentá de nuevo.</p>
-      `;
-        }
-  });
+  } catch {
+    resultado.innerHTML = `<h2>Pokémon no encontrado</h2>`;
+  }
 }
-
-mostrarPokemon();
